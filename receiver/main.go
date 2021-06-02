882731d5afdbff5a767a6d1757b3c4eb193d7dce
Robbie08 <rortiz8838@sdsu.edu>
2021-06-01 21:35:40 -0700

    Implemented reading abilities to Receiver program.

--- receiver/main.go
+++ receiver/main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
+	"bufio"
 )
+
+
 
 // write code for receiver here
 func main(){
-	log.Println("Hello from Receiver")
+
+	file, err := os.Open("generator_output.txt") // read our generator_output file in
+
+	// handle event where the file cannot open correctly
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close() // when the program is terminated, close our file
+
+	scanner := bufio.NewScanner(file) // create our file handler object
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	} 
+
+	// if there is an issue while reading the file
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
 }
